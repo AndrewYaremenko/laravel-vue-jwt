0a5c77b4f73350efebdf76c9b0e745621ea2f8a6
unknown <esdesuagk@gmail.com>
2023-08-18 23:45:41 +0300

    laravel/ui(bootstrap, vue, auth, vue-router)

---
+++ resources/js/components/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <router-link :to="{ name: 'fruits.index' }">List</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+    name: "Index"
+}
+</script>
+<style>
+    
+</style>
